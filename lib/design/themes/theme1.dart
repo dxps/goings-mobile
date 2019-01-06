25a0a1b8a027ebced0bc7da59e1e594e53866319
vision8tech <vision8r@gmail.com>
2019-01-06 19:23:47 +0200

    [+] colorset & font

---
+++ lib/design/themes/theme1.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+//
+/// "Sunny and Calm" theme.
+/// (#49 at https://learn.canva.com/learn/website-color-schemes/)
+//
+class Theme1 {
+  //
+
+  //
+  static MaterialColor honeyShades() {
+    //
+    return MaterialColor(0xFFDCAE1D, <int, Color>{
+      50: Color(0xFFfbf5e4),
+      100: Color(0xFFf5e7bb),
+      200: Color(0xFFeed78e),
+      300: Color(0xFFe7c661),
+      400: Color(0xFFe1ba3f),
+      500: Color(0xFFdcae1d),
+      600: Color(0xFFd8a71a),
+      700: Color(0xFFd39d15),
+      800: Color(0xFFce9411),
+      900: Color(0xFFc5840a),
+    });
+  }
+
+  //
+  static Color honeyColor() {
+    //
+    return Color(0xFFdcae1d);
+  }
+
+  //
+  static Color ceruleanColor() {
+    //
+    return Color(0xFF00303F);
+  }
+
+  //
+  static MaterialColor ceruleanShades() {
+    //
+    return MaterialColor(0xFF00303F, <int, Color>{
+      50: Color(0xFFe0e6e8),
+      100: Color(0xFFb3c1c5),
+      200: Color(0xFF80989f),
+      300: Color(0xFF4d6e79),
+      400: Color(0xFF264f5c),
+      500: Color(0xFF00303f),
+      600: Color(0xFF002b39),
+      700: Color(0xFF002431),
+      800: Color(0xFF001e29),
+      900: Color(0xFF00131b),
+    });
+  }
+
+  //
+  static Color morningSkyColor() {
+    //
+    return Color(0xFFCAE4DB);
+  }
+
+  //
+  static MaterialColor morningSkyShades() {
+    //
+    return MaterialColor(0xFFCAE4DB, <int, Color>{
+      50: Color(0xFFf9fcfb),
+      100: Color(0xFFeff7f4),
+      200: Color(0xFFe5f2ed),
+      300: Color(0xFFdaece6),
+      400: Color(0xFFd2e8e0),
+      500: Color(0xFFcae4db),
+      600: Color(0xFFc5e1d7),
+      700: Color(0xFFbdddd2),
+      800: Color(0xFFb7d9cd),
+      900: Color(0xFFabd1c4),
+    });
+  }
+
+  //
+  static Color mistColor() {
+    //
+    return Color(0xFF7A9D96);
+  }
+
+  //
+  static MaterialColor mistShades() {
+    //
+    return MaterialColor(0xFF7A9D96, <int, Color>{
+      50: Color(0xFFeff3f2),
+      100: Color(0xFFd7e2e0),
+      200: Color(0xFFbdcecb),
+      300: Color(0xFFa2bab6),
+      400: Color(0xFF8eaca6),
+      500: Color(0xFF7a9d96),
+      600: Color(0xFF72958e),
+      700: Color(0xFF678b83),
+      800: Color(0xFF5d8179),
+      900: Color(0xFF4a6f68),
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
 
+import './design/themes/theme1.dart';
+
 void main() => runApp(MyApp());
 
+//
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  //
+
   @override
   Widget build(BuildContext context) {
+    //
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Goings',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        // accentColor: Colors.yellow,
+        brightness: Brightness.light,
+        canvasColor: Theme1.morningSkyColor(),
+        // primaryColor: Colors.yellow,
+        primarySwatch: Theme1.honeyShades(),
+        fontFamily: 'Nunito',
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Goings'),
     );
   }
 }
@@ -69,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title, style: TextStyle(color: Theme1.ceruleanColor())),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
